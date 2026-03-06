94cc40474ff41ce5e08fac2f0be705bf50080637
Juan Pablo Reyes Ruiz <l23330101@hermosillo.tecnm.mx>
2026-03-05 17:23:25 -0700

    P-1.2 sin video

--- 1_Salida_Digital/Salida_Digital.ino
+++ 1_Salida_Digital/Salida_Digital.ino
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 
 // byte usa 8 bits (0 a 255)
-byte ledPin = 3;
+byte ledPin1 = 3;
+byte ledPin2 = 5;
 // int usa 16 bits (-32,768 a 32,767)
 int tiempoEncendido = 200;  //milisegundos
 // uint16_t (unsigned int) usa 16 bits (0 a 65,535)
@@ -11,17 +12,21 @@ uint16_t tiempoApagado = 800;    //milisegundos
 // long, float, double, etc.
 
 void setup() {
-    pinMode(ledPin, OUTPUT);
+    pinMode(ledPin1, OUTPUT);
+    pinMode(ledPin2, OUTPUT);
 }
 
 void loop() {
     // HIGH es el valor lógico 1/true o 5v,
     // LOW es 0/false o 0v
-    digitalWrite(ledPin, HIGH);
+    digitalWrite(ledPin1, HIGH);
+    digitalWrite(ledPin2, LOW);
+    delay(300); 
     // delay() recibe un valor en milisegundos
     // el programa se detiene por el tiempo indicado
-    delay(tiempoEncendido);
+    
 
-    digitalWrite(ledPin, LOW);
-    delay(tiempoApagado);
+    digitalWrite(ledPin1, LOW);
+    digitalWrite(ledPin2, HIGH);
+    delay(500);
 }
