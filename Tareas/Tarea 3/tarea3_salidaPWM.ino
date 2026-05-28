2cb1119f72a3f47596fbc3aaf1278cd6ad72ba81
Alan Ballesteros <l23330015@hemosillo.tecnm.mx>
2026-05-28 10:23:29 -0700

    Add initial implementations for LED control in Tarea 1, Tarea 2, and Tarea 3

---
+++ Tareas/Tarea 3/tarea3_salidaPWM.ino
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <math.h>
+
+const byte pinLed = 3;
+const float periodoMs = 1000.0; 
+
+void setup() {
+    pinMode(pinLed, OUTPUT);
+}
+
+void loop() {
+    unsigned long t = millis();
+
+    // Cálculo de la onda senoidal en base al tiempo
+    float angulo = 2.0 * PI * (t % (unsigned long)periodoMs) / periodoMs;
+    float senoNormalizado = (sin(angulo) + 1.0) * 0.5;
+    byte valorPWM = (byte)(senoNormalizado * 255.0);
+
+    analogWrite(pinLed, valorPWM);
+    delay(10); 
+}
